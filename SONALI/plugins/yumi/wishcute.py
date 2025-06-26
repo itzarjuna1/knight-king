18774699f19617eff5a9daa6c08612a55c308185
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-06-27 01:08:01 +0530

    Update wishcute.py

--- SONALI/plugins/yumi/wishcute.py
+++ SONALI/plugins/yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from SONALI import app 
 
-SUPPORT_CHAT = "PURVI_SUPPORT"
+SUPPORT_CHAT = "Destiny_Infinity_Og"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
