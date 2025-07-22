08460e8db9a811a30dbc8d7dbc6a23fe97400737
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-07-23 02:24:43 +0530

    Update call.py

--- SONALI/core/call.py
+++ SONALI/core/call.py
@@ -8,7 +8,7 @@ from pyrogram.types import InlineKeyboardMarkup
 
 from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
-from pytgcalls.types.stream import StreamAudioEnded
+#from pytgcalls.types.stream import StreamAudioEnded
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
 from pytgcalls.exceptions import AlreadyJoinedError, NoActiveGroupCall
