97a6e624c780c287bd52e26c18ca9b84cde32fe3
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-07-23 02:26:44 +0530

    Update call.py

--- SONALI/core/call.py
+++ SONALI/core/call.py
@@ -5,13 +5,16 @@ from typing import Union
 
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
-
-from pytgcalls import PyTgCalls
+from pytgcalls import PyTgCalls, StreamType
+from pytgcalls.exceptions import (
+    AlreadyJoinedError,
+    NoActiveGroupCall,
+    TelegramServerError,
+)
 from pytgcalls.types import Update
-from pytgcalls.types.input_streams import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_streams.quality import HighQualityAudio, MediumQualityVideo
-from pytgcalls.exceptions import AlreadyJoinedError, NoActiveGroupCall
-
+from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
+from pytgcalls.types.stream import StreamAudioEnded
 import config
 from SONALI import LOGGER, YouTube, app
 from SONALI.misc import db
