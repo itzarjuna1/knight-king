3f4f2e2b60ccd099bbf0dc774e9289bb21e3d6d5
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-07-23 12:43:21 +0530

    Update call.py

--- SONALI/core/call.py
+++ SONALI/core/call.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
 
-from pytgcalls import PyTgCalls
-from pytgcalls.types import StreamType
+from py_tgcalls import PyTgCalls
+from py_tgcalls.types.stream import StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
 from pytgcalls.types.stream import StreamAudioEnded
